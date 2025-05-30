afa92a8c07884f0d60f1f1b228885bbf5409f78b
lovenzola <nzolalove435@gmail.com>
2025-05-30 11:54:18 +0100

    Ajout d'un fichier js

---
+++ script.js
@@ -0,0 +1,7 @@
+const burger = document.getElementByClassName(".burger");
+const navbar = document.getElementByClassName(".navbar");
+const closeburger = document.getElementByClassName(".clic");
+
+burger.addEventListener("click", ()=> {
+    navbar.Classlist.toogle(flex);
+})
